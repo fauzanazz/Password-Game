8115d7efdfe168cdb879d4a4438200c8566dea37
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-07-10 03:45:48 +0700

    feat : Auto Size Text

--- components/playerinput.tsx
+++ components/playerinput.tsx
@@ -1,13 +1,46 @@
 "use client";
-import {useState} from "react";
+import React, {useState, ChangeEvent, useRef} from "react";
+import useAutosizeText from "@/components/AutoSizeText";
 
 const PlayerInput = () => {
+    const inputRef = useRef<HTMLTextAreaElement>(null);
     const [password, setPassword] = useState<string>("");
+    const [level, setLevel] = useState<number>(0);
+    const [isStarted, setIsStarted] = useState<boolean>(false);
+    const [isFinished, setIsFinished] = useState<boolean>(false);
+    const [passlength, setPasslength] = useState<number>(0);
+
+    useAutosizeText(inputRef.current, password);
+
+    const handleChange = async (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        if (!isStarted) {
+            setIsStarted(true);
+        }
+
+        const input = event.target?.value;
+        setPassword(input);
+        setPasslength(input.length);
+
+        console.log(level);
+    }
+
     return (
-        <div className="text-center">
-            <h1 className="text-4xl font-serif mb-8">✶ The Password Game</h1>
-            <label className="block text-lg mb-2">Please choose a password</label>
-            <input className="border rounded-lg p-2 w-96" onChange={(e) => setPassword(e.target.value)} />
+        <div>
+            <div className="text-center">
+                <h1 className="text-4xl font-serif mb-8">✶ The Password Game</h1>
+                <label className="block text-lg mb-2">Please choose a password</label>
+                <div className="flex justify-center ">
+                    <textarea
+                        onChange={handleChange}
+                        placeholder="Enter your password"
+                        ref={inputRef}
+                        rows={1}
+                        value={password}
+                        className="border rounded-lg p-2 w-96"
+                    />
+                    <label className="text-lg ml-2 p-2"> {passlength === 0 ? "" : passlength} </label>
+                </div>
+            </div>
         </div>
     );
 };
